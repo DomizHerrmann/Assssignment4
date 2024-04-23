579bf38f43be69a329f5e952bf848f9da80f96ce
Dominik Herrmann <146055286+DomizHerrmann@users.noreply.github.com>
2024-04-23 15:38:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
     <title>Webapp Status</title>
 </head>
 <body>
-    <h1><?php echo 'The webapp is running.'; ?></h1>
+    <h1><?php echo 'The webapp is running. And these are some changes that were deployed automatically.'; ?></h1>
 </body>
 </html>
